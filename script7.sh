f7f156484579b79ab1c23b25e4f0dbeebec77f4c
Kashinath185 <58341030+Kashinath185@users.noreply.github.com>
2022-04-18 19:16:24 +0530

    Update script7.sh

--- script7.sh
+++ script7.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+var()
+
+{
+Read in one character from STDIN.
+If the character is 'Y' or 'y' display "YES".
+If the character is 'N' or 'n' display "NO".
+No other character will be provided as input.
+
+}
+
 read t
 
 if [ $t == 'Y' ] || [ $t == 'y' ];
